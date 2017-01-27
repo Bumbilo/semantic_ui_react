9dfb0041fd620699b7efa3f190dd0d411edb0675
bumbilo <dimagolyak@gmail.com>
2017-01-27 23:31:17 +0200

    add modalt dir

--- src/components/App.js
+++ src/components/App.js
@@ -3,11 +3,12 @@ import Login from './forms/login_form';
 import LoginForgot from './forms/forgot_form';
 import Header from './header/header_main';
 import LoginReset from './forms/reset_form';
+import ModalWindow from './modal/modal';
 import {Container} from 'semantic-ui-react';
 
 class App extends React.Component {
     render() {
-        return (<Login/>);
+        return (<ModalWindow/>);
     }
 }
 
---
+++ src/components/modal/modal.js
@@ -0,0 +1,22 @@
+import React, {Component} from 'react';
+import { Button, Header, Icon, Modal } from 'semantic-ui-react';
+import './modal.css';
+
+const ModalWindow = () => (
+  <Modal trigger={<Button>Show Modal</Button>} closeIcon='close'>
+    <Header icon='archive' content='Archive Old Messages' />
+    <Modal.Content>
+      <p>Your inbox is getting full, would you like us to enable automatic archiving of old messages?</p>
+    </Modal.Content>
+    <Modal.Actions>
+      <Button color='red'>
+        <Icon name='remove' /> No
+      </Button>
+      <Button color='green'>
+        <Icon name='checkmark' /> Yes
+      </Button>
+    </Modal.Actions>
+  </Modal>
+)
+
+export default ModalWindow ;
