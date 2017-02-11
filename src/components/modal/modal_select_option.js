0d66e43349a97f5aa8c22eb250bee53a61161227
bumbilo <dimagolyak@gmail.com>
2017-02-11 10:29:11 +0200

    added modal_select_option

--- src/components/modal/modal_select_option.js
+++ src/components/modal/modal_select_option.js
@@ -1,20 +1,19 @@
 import React, {Component} from 'react';
-import {
-    Button,
-    Modal,
-} from 'semantic-ui-react';
+import {Button, Modal} from 'semantic-ui-react';
 import '../../CSS/fonts.css';
 import './modal.css';
 
 const AddNewEmployee = () => (
     <div className='add_new_employee'>
-        <h1>Hello world</h1>
+        <span className="title_small_modal">Add new employee</span>
+        <span className="sub_title_modal">Add new employee</span>
     </div>
 );
 
 const TimeOfRequest = () => (
-    <div className='add_new_employee'>
-        <h1>Why it's dont work !!!</h1>
+    <div className='add_new_employee2'>
+        <span className="title_small_modal">Time-off request</span>
+        <span className="sub_title_modal">Time off request required approval</span>
     </div>
 );
 
@@ -23,7 +22,10 @@ const TitleModal = () => (
 );
 
 const ModalSelectOption = () => (
-    <Modal trigger={<Button> Show Modal </Button>} closeIcon='close' className='modal_select'>
+    <Modal
+        trigger={<Button> Show Modal </Button>}
+        closeIcon='close'
+        className='modal_select'>
         <TitleModal/>
         <AddNewEmployee/>
         <TimeOfRequest/>
